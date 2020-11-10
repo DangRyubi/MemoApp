a13d42963c00eaf5e2a63cfe8a5a7f23a348dfab
LCP_Tokyo_Macbook <lcp_tokyo_macbook@LCP-Tokyo-MacbooknoMacBook-Pro.local>
2020-11-10 14:42:14 +0900

    Create MemoEditScreen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import Appbar from './src/components/AppBar';
 
 export default class App extends React.Component {
@@ -9,7 +9,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoListScreen />
+        <MemoEditScreen />
       </View>
     );
   }
---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CicleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.memoEditInput} multiline="True" value="内容を書いてください" />
+        <CicleButton name="check" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    backgroundColor: '#ddd',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+  },
+});
+
+export default MemoEditScreen;

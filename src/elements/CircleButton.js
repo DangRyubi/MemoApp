4cdba18a6f299a89edd44d82b4c7884cc1ec7a69
LCP_Tokyo_Macbook <lcp_tokyo_macbook@LCP-Tokyo-MacbooknoMacBook-Pro.local>
2020-11-10 11:13:44 +0900

    Style MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
-    return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonTitle}>
+    const { style, color } = this.props;
+
+    let bgColor = '#D74B4B';
+    let textColor = '#FFF';
+
+    if (color === 'white') {
+      bgColor = '#FFF';
+      textColor = '#D74B4B';
+    }
+
+    return ( // 左、右順で上書き
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
           {this.props.children}
         </Text>
       </View>
@@ -20,7 +30,6 @@ const styles = StyleSheet.create({
     right: 32,
     width: 56,
     height: 56,
-    backgroundColor: '#D74B4B',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 28,
